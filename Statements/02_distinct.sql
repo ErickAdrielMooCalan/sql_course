41d6c29832ba6263702c17977d36dfd903797708
Erick Adriel Moo Calan <112136002+ErickAdrielMooCalan@users.noreply.github.com>
2024-07-15 15:57:34 -0500

    Add 02_distinct.sql

---
+++ Statements/02_distinct.sql
@@ -0,0 +1,5 @@
+--Select and show all distinct items
+SELECT DISTINCT * FROM users;
+
+--Select and show only the different items, if exist any different it won't show
+SELECT DISTINCT age FROM users;

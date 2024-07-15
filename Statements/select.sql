95f37f76b4e1d17f74800c66e8070fc4e34d9bcd
Erick Adriel Moo Calan <112136002+ErickAdrielMooCalan@users.noreply.github.com>
2024-07-15 15:36:03 -0500

    Create select.sql

---
+++ Statements/select.sql
@@ -0,0 +1,8 @@
+--Selection from users table and show all items
+SELECT * FROM  users;
+
+--Selection the column called "first_name" and show all items from that column
+SELECT first_name FROM users;
+
+--Selection the columns called "id_users" and "first_name" and show all items from those columns
+SELECT id_users, first_name FROM users;

48dcf6f7b8423c0d1019ebff53a1f4c490ac49af
Erick Adriel Moo Calan <112136002+ErickAdrielMooCalan@users.noreply.github.com>
2024-07-17 16:22:55 -0500

    Add 02_update.sql

--- 02_Database/01_insert_into.sql
+++ 02_Database/01_insert_into.sql
@@ -1,7 +1,9 @@
+--Create a new user in the users table
 INSERT INTO  users (id_users,  first_name, last_name) VALUES(
      12, 'Maria', 'Cauich'
 );
 
+--Create a new user in the users table without specifying the ID
 INSERT INTO  users (first_name, last_name) VALUES(
      'Francisco', 'Ochoa'
 );
---
+++ 02_Database/02_update.sql
@@ -0,0 +1,5 @@
+--Update the age of a user with ID 3
+UPDATE users SET age = 21 WHERE id_users = 3;
+
+--Update the age and the init date of a user with ID 2
+UPDATE users SET age = 20, init_date = '2016-01-22' WHERE id_users = 2;

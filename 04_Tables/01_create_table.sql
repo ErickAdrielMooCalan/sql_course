27369642fa6c74ceb4360cea707bfc39f8647ebe
Erick Adriel Moo Calan <112136002+ErickAdrielMooCalan@users.noreply.github.com>
2024-07-23 16:34:50 -0500

    Add 01_create_table.sql

---
+++ 04_Tables/01_create_table.sql
@@ -0,0 +1,85 @@
+--Create new table named "person"
+CREATE TABLE person(
+    id_person INT,
+    name VARCHAR(100),
+    age INT,
+    email VARCHAR(100),
+    created DATE
+);
+
+--Create new table named "person_2" with with the constraint "NOT NULL"
+CREATE TABLE person_2(
+    id_person INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    age INT,
+    email VARCHAR(100),
+    created DATE
+);
+
+--Create new table named "person_3" with with the constraint "NOT NULL" and "UNIQUE"
+CREATE TABLE person_3(
+    id_person INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    age INT,
+    email VARCHAR(100),
+    created DATETIME,
+    UNIQUE(id_person)
+);
+
+--Create new table named "person_4" with with the constraint "NOT NULL", "UNIQUE" and "PRIMARY KEY"
+CREATE TABLE person_4(
+    id_person INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    age INT,
+    email VARCHAR(100),
+    created DATETIME,
+    UNIQUE(id_person),
+    PRIMARY KEY(id_person)
+);
+
+--Create new table named "person_5" with with the constraint "NOT NULL", "UNIQUE", "PRIMARY KEY" and "CHECK"
+CREATE TABLE person_5(
+    id_person INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    age INT,
+    email VARCHAR(100),
+    created DATETIME,
+    UNIQUE(id_person),
+    PRIMARY KEY(id_person),
+    CHECK(age >= 18)
+);
+
+/*
+    Create new table named "person_6" with with the constraint "NOT NULL", "UNIQUE", "PRIMARY KEY",
+    "CHECK" and "DEFAULT"
+*/
+CREATE TABLE person_6(
+    id_person INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    age INT,
+    email VARCHAR(100),
+    created DATETIME DEFAULT CURRENT_TIMESTAMP(),
+    UNIQUE(id_person),
+    PRIMARY KEY(id_person),
+    CHECK(age >= 18)
+);
+
+/*
+    Create new table named "person_7" with with the constraint "NOT NULL", "UNIQUE", "PRIMARY KEY",
+    "CHECK", "DEFAULT" and "AUTO_INCREMENT"
+*/
+CREATE TABLE person_7(
+    id_person INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    age INT,
+    email VARCHAR(100),
+    created DATETIME DEFAULT CURRENT_TIMESTAMP(),
+    UNIQUE(id_person),
+    PRIMARY KEY(id_person),
+    CHECK(age >= 18)
+);
+
+--Test table
+CREATE TABLE person_test(
+    name VARCHAR(100) NOT NULL
+);

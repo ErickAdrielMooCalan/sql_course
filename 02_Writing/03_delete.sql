dfb140104532f4bb3af6fe529e99224ea8d0dbec
Erick Adriel Moo Calan <112136002+ErickAdrielMooCalan@users.noreply.github.com>
2024-07-23 13:41:41 -0500

    Add folder "03_Database" and file "01_create.sql"

--- 02_Writing/03_delete.sql
+++ 02_Writing/03_delete.sql
@@ -1,1 +1,2 @@
+--Delete user with ID = 6
 DELETE FROM users WHERE id_users = 6;
---
+++ 03_Database/01_create.sql
@@ -0,0 +1,5 @@
+/*
+    Note: DATABASE and SCHEMA are the SAME because they are both containers.
+*/
+
+CREATE DATABASE test_db;

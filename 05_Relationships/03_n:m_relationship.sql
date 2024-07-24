3a8f683ff98ae6d385c0068b02f7588eab5d2043
Erick Adriel Moo Calan <112136002+ErickAdrielMooCalan@users.noreply.github.com>
2024-07-24 17:31:38 -0500

    Add 03_n:m_relationship.sql

---
+++ 05_Relationships/03_n:m_relationship.sql
@@ -0,0 +1,15 @@
+--To start, we create a new table called "language"
+CREATE TABLE languages(
+    id_language INT AUTO_INCREMENT PRIMARY KEY,
+    name_language VARCHAR(30)NOT NULL
+);
+
+--Next we create another table to hold the foreign keys of two main tables.
+CREATE TABLE users_languages(
+    id_users_language INT AUTO_INCREMENT PRIMARY KEY,
+    fk_id_users INT,
+    fk_id_language INT,
+    FOREIGN KEY(fk_id_users) REFERENCES users(id_users),
+    FOREIGN KEY(fk_id_language) REFERENCES languages(id_language),
+    UNIQUE(fk_id_users, fk_id_language)
+);

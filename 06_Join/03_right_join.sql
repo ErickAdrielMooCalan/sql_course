48f3161e53fd9a123ae6b75e7d4d35d01d611d07
erickmoo <erickmoo@localhost.localdomain>
2024-07-30 16:31:12 -0500

    Add 03_right_join.sql

---
+++ 06_Join/03_right_join.sql
@@ -0,0 +1,37 @@
+--Show right join between "users" and "dni" tables
+SELECT * FROM users
+--Which table will it join? R = dni
+RIGHT JOIN dni
+ON users.id_users = dni.fk_id_users;
+
+/*
+    Show the right join between the "users" and "dni" tables, but only shows the "first name" and
+    "dni number" columns
+*/
+SELECT users.first_name, dni.dni_number FROM users
+--Which table will it join? R = dni
+RIGHT JOIN dni
+ON users.id_users = dni.fk_id_users;
+/*
+    Simple form
+
+    SELECT first_name, dni_number FROM users
+    RIGHT JOIN dni
+    ON users.id_users = dni.fk_id_users;
+*/
+
+--Show right join between the "dni" and "users" tables (reverse order)
+SELECT dni.dni_number, users.first_name FROM dni
+--Which table will it join? R = users
+RIGHT JOIN users
+ON users.id_users = dni.fk_id_users;
+
+--Show users who know a programming language using right join
+SELECT users.first_name, languages.name_language
+FROM users
+--Which table will it join? R = users_languages
+RIGHT JOIN users_languages
+ON users.id_users = users_languages.fk_id_users
+--Which table will it join? R = users_languages
+RIGHT JOIN languages
+ON users_languages.fk_id_language = languages.id_language;
